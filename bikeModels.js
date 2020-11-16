b8767643a0710e5a947d9c1dabe5133e83be0281
Gregory.Johnson <3531op@gmail.com>
2020-11-16 14:24:16 -0500

    Logic update

--- bikeModels.js
+++ bikeModels.js
@@ -1,29 +1,39 @@
 const bikeModels = [
-{
-make: 'honda',
-model: 'crf450r'
-},
+  {
+    make: 'honda',
+    model: 'crf450r'
+  },
 
-{
-make: 'yamaha',
-model: 'yz250',
-},
+  {
+    make: 'yamaha',
+    model: 'yz250',
+  },
 
-{
-make: 'kawasaki',
-model: 'kx450f'
-},
+  {
+    make: 'kawasaki',
+    model: 'kx450f'
+  },
 
-{
-make: 'suzuki', 
-model: 'rmz250',
-},
+  {
+    make: 'suzuki',
+    model: 'rmz250',
+  },
 
-{
-make: 'ktm',
-model: '300xc',
-}
+  {
+    make: 'ktm',
+    model: '300xc',
+  },
 
+  {
+    make: 'husqvarna',
+    model: '300tx',
+  },
+
+  {
+    make: 'gasgas',
+    model: 'ec300',
+  }
+  
 ]
 
 
--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 const readline = require('readline')
 const getRandom = require('./randomMake.js')
 const bikeModels = require('./bikeModels.js')
-const randomNess = getRandom(bikeModels)
-const randomModel = randomNess.model
-const randomMake = randomNess.make
-let newBike = getRandom(bikeModels)
-let newModel = newBike.model
-let newMake = newBike.make
-let newerBike = getRandom(bikeModels)
-let newerModel = newerBike.model
-let newerMake = newerModel.make
+const bike = getRandom(bikeModels)
+const newBike = getRandom(bikeModels)
+const newerBike = getRandom(bikeModels)
+const newestBike = getRandom(bikeModels)
+const finalBike = getRandom(bikeModels)
+
 
 const interface = readline.createInterface({
   input: process.stdin,
@@ -17,36 +14,31 @@ const interface = readline.createInterface({
 })
 
 
-let passTest = 0
 let failTest = 0
 const question = function (userInput) {
-  if (failTest === 2) {
-    console.log(`You suck!! Bye Bye!`)
+  if (userInput.toLowerCase() === bike.make) {
+    console.log("That's correct.\n")
+    interface.question(`Who makes the ${newBike.model}?\n`, question2)
+    console.log(`Type your answer below:\n`)
+  } else if (failTest === 2) {
+    console.log(`Sorry. You have answered incorrectly 3 times. You have failed!`)
     interface.close()
-  } else if (userInput === newMake) {
-    console.log("That's correct.")
-    interface.question(`Who makes the ${newModel}?\n`, question2)
-    passTest++
-  } else if (userInput !== newMake) {
+  } else if (userInput.toLowerCase() !== bike.make) {
     interface.question(`That's not correct. Please try again.\n`, question)
     console.log(`Type your answer below:\n`)
     failTest++
   }
-
 }
 
 const question2 = function (userInput) {
-  if (passTest === 2) {
-    console.log(`You passed the test!`)
-    interface.close()
+  if (userInput.toLowerCase() === newBike.make) {
+    console.log("That's correct.\n")
+    interface.question(`ho makes the ${newerBike.model}?\n`, question3)
+    console.log(`Type your answer below:\n`)
   } else if (failTest === 2) {
-    console.log(`You suck!! Bye Bye!`)
+    console.log(`Sorry. You have answered incorrectly 3 times. You have failed!`)
     interface.close()
-  } else if (userInput === randomMake) {
-    console.log("That's correct.")
-    interface.question(`Who makes the ${newerModel}?\n`, question3)
-    passTest++
-  } else if (userInput !== randomMake) {
+  } else if (userInput.toLowerCase() !== newBike.make) {
     interface.question(`That's not correct. Please try again.\n`, question2)
     console.log(`Type your answer below:\n`)
     failTest++
@@ -55,16 +47,14 @@ const question2 = function (userInput) {
 }
 
 const question3 = function (userInput) {
-  if (passTest === 2) {
-    console.log(`You passed the test!`)
-    interface.close()
+  if (userInput.toLowerCase() === newerBike.make) {
+    console.log("That's correct.\n")
+    interface.question(`Who makes the ${newestBike.model}?\n`, question4)
+    console.log(`Type your answer below:\n`)
   } else if (failTest === 2) {
-    console.log(`You suck!! Bye Bye!`)
+    console.log(`Sorry. You have answered incorrectly 3 times. You have failed!`)
     interface.close()
-  } else if (userInput === newerMake) {
-    console.log("That's correct.")
-    passTest++
-  } else if (userInput !== newerMake) {
+  } else if (userInput.toLowerCase() !== newerBike.make) {
     interface.question(`That's not correct. Please try again.\n`, question3)
     console.log(`Type your answer below:\n`)
     failTest++
@@ -72,18 +62,55 @@ const question3 = function (userInput) {
 
 }
 
+const question4 = function (userInput) {
+  if (userInput.toLowerCase() === newestBike.make) {
+    console.log(`That's correct.\n`)
+    interface.question(`Final question! Who makes the ${finalBike.model}?\n`, question5)
+    console.log(`Type your answer below:\n`)
+  } else if (failTest === 2) {
+    console.log(`Sorry. You have answered incorrectly 3 times. You have failed!`)
+    interface.close()
+  } else if (userInput.toLowerCase() !== newerBike.make) {
+    interface.question(`That's not correct. Please try again.\n`, question4)
+    console.log(`Type your answer below:\n`)
+    failTest++
+  }
 
+}
 
+const question5 = function (userInput) {
+ if (userInput.toLowerCase() === finalBike.make) {
+    console.log("You passed the test!.\n")
+    console.log(`Great Job! You're a natural!`)
+    interface.close()
+  } else if (failTest === 2) {
+    console.log(`Sorry. You have answered incorrectly 3 times. You have failed!`)
+    interface.close()
+  } else if (userInput.toLowerCase() !== newerBike.make) {
+    interface.question(`That's not correct. Please try again.\n`, question5)
+    console.log(`Type your answer below:\n`)
+    failTest++
+  }
 
+}
 
+console.log(`Welcome all to a fun little quiz. You will be given 5 different modern dirt bike models. 
+You must answer with the correct manufacturer for that model. If you fail to answer correctly 3 times you will be kicked
+from the test and a curse shall be placed upon yee... If you happen to get all 3 correct well... good for you. Let's go ride!\n`)
 
-
-
-
-
-
-interface.question(`Who makes this bike ${randomModel}?\n`, question)
+interface.question(`Who makes the ${bike.model}?\n`, question)
 console.log(`Type your answer below:\n`)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
